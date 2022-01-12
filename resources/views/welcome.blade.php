92352acb044dd5488509e41794ea6432651c18dc
haafidz-jp <haafidz123@gmail.com>
2022-01-12 11:52:29 +0700

    update landing page content.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Home</title>
+    <title>CV. MULIA JAYA AGUNG | LANDING PAGE</title>
 
     <!-- Favicons -->
     <link href="{{ asset('company/img/favicon.png') }}" rel="icon">
@@ -35,8 +35,8 @@
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
         <a href="" class="logo d-flex align-items-center">
-            <img src="{{ url('company/img/hero-bg.png') }}" alt="">
-            <span>Mulia Jaya</span>
+            {{-- <img src="{{ url('company/img/hero-bg.png') }}" alt=""> --}}
+            <span>CV. Mulia Jaya Agung</span>
         </a>
 
         <nav id="navbar" class="navbar">
@@ -289,143 +289,183 @@
     <section id="contact" class="contact">
 
         <div class="container" data-aos="fade-up">
-
-            <header class="section-header">
-                <h2>Contact</h2>
-                <p>Contact Us</p>
-            </header>
-
-            <div class="row gy-4">
-
-                <div class="col-lg-6">
-
-                    <div class="row gy-4">
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-geo-alt"></i>
-                                <h3>Address</h3>
-                                <p>A108 Adam Street,<br>New York, NY 535022</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-telephone"></i>
-                                <h3>Call Us</h3>
-                                <p>+1 5589 55488 55<br>+1 6678 254445 41</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-envelope"></i>
-                                <h3>Email Us</h3>
-                                <p>info@example.com<br>contact@example.com</p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="info-box">
-                                <i class="bi bi-clock"></i>
-                                <h3>Open Hours</h3>
-                                <p>Monday - Friday<br>9:00AM - 05:00PM</p>
-                            </div>
-                        </div>
-                    </div>
-
+  
+          <header class="section-header">
+            <h2>Contact</h2>
+            <p>Contact Us</p>
+          </header>
+  
+          <div class="row gy-4">
+  
+            <div class="col-lg-12">
+  
+              <div class="row gy-4">
+                <div class="col-md-3">
+                  <div class="info-box">
+                    <i class="bi bi-geo-alt"></i>
+                    <h3>Our Address</h3>
+                    <p>Jl.Pintu Pasar Barat No.36,<br>Jatinegara, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13310<br><br></p>
+                  </div>
                 </div>
-
-                <div class="col-lg-6">
-                    <form action="forms/contact.php" method="post" class="php-email-form">
-                        <div class="row gy-4">
-
-                            <div class="col-md-6">
-                                <input type="text" name="name" class="form-control" placeholder="Your Name" required>
-                            </div>
-
-                            <div class="col-md-6 ">
-                                <input type="email" class="form-control" name="email" placeholder="Your Email" required>
-                            </div>
-
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="subject" placeholder="Subject" required>
-                            </div>
-
-                            <div class="col-md-12">
-                                <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
-                            </div>
-
-                            <div class="col-md-12 text-center">
-                                <div class="loading">Loading</div>
-                                <div class="error-message"></div>
-                                <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                                <button type="submit">Send Message</button>
-                            </div>
-
-                        </div>
-                    </form>
-
+                <div class="col-md-3">
+                  <div class="info-box">
+                    <i class="bi bi-telephone"></i>
+                    <h3>Phone</h3>
+                    <p>(021) 850 9279<br>
+                      <a href="https://wa.me/6282298399292?text=Hallo%20Admin">+62 822 9839 9292</a> (Whatsapp)<br>
+                      <a href="https://wa.me/6281288253426?text=Hallo%20Admin">+62 812 8825 3426</a> (Whatsapp)<br><br></p>
+                  </div>
                 </div>
-
+                <div class="col-md-3">
+                  <div class="info-box">
+                    <i class="bi bi-envelope"></i>
+                    <h3>Email</h3>
+                    <p>info@muliajaya.xyz<br>admin@muliajaya.xyz<br><br><br></p>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="info-box">
+                    <i class="bi bi-clock"></i>
+                    <h3>Bussiness Hour</h3>
+                    <p>Senin - Sabtu<br>07:00 - 16:00 WIB</p>
+                    <p>Minggu<br>07:00 - 15:00 WIB</p>
+                  </div>
+                </div>
+              </div>
+  
             </div>
-
+  
+            <!-- <div class="col-lg-6">
+              <form action="forms/contact.php" method="post" class="php-email-form">
+                <div class="row gy-4">
+  
+                  <div class="col-md-6">
+                    <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+                  </div>
+  
+                  <div class="col-md-6 ">
+                    <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+                  </div>
+  
+                  <div class="col-md-12">
+                    <input type="text" class="form-control" name="subject" placeholder="Subject" required>
+                  </div>
+  
+                  <div class="col-md-12">
+                    <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+                  </div>
+  
+                  <div class="col-md-12 text-center">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your message has been sent. Thank you!</div>
+  
+                    <button type="submit">Send Message</button>
+                  </div>
+  
+                </div>
+              </form>
+  
+            </div> -->
+  
+          </div>
+  
         </div>
-
-    </section><!-- End Contact Section -->
+  
+      </section><!-- End Contact Section -->
 
 </main><!-- End #main -->
 
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
-    <div class="footer-top">
-        <div class="container">
-            <div class="row gy-4">
-                <div class="col-lg-5 col-md-12 footer-info">
-                    <a href="index.html" class="logo d-flex align-items-center">
-                        <img src="assets/img/logo.png" alt="">
-                        <span>Mulia Jaya</span>
-                    </a>
-                </div>
 
-                <div class="col-lg-2 col-6 footer-links">
-                    <h4>Useful Links</h4>
-                    <ul>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Product</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-2 col-6 footer-links">
-                    <h4>Our Services</h4>
-                    <ul>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Shop</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-                    <h4>Contact Us</h4>
-                    <p>
-                        A108 Adam Street <br>
-                        New York, NY 535022<br>
-                        United States <br><br>
-                        <strong>Phone:</strong> +1 5589 55488 55<br>
-                        <strong>Email:</strong> info@example.com<br>
-                    </p>
-
-                </div>
-
-            </div>
+    <!-- START NEWSLETTER -->
+    <!-- <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+          </div>
+          <div class="col-lg-6">
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
         </div>
+      </div>
+    </div> -->
+    <!-- END NEWSLETTER -->
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-4 col-md-12 footer-info">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <!-- <img src="assets/img/logo.png" alt=""> -->
+              <span>CV. Mulia Jaya Agung</span>
+            </a>
+            <p class="mt-4">Solusi Alat Tulis Kantor anda!</p>
+            <div class="social-links mt-3">
+              <a href="https:instagram.com/cvmuliajaya" target="blank" class="instagram"><i class="bi bi-instagram"></i></a>
+              <!-- <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> -->
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#hero">Home</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#about">About</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#portfolio">Product</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="/shops">Shop</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+
+          <!-- <div class="col-lg-2 col-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div> -->
+
+          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Contact Us</h4>
+            <p>
+              Jl.Pintu Pasar Barat No.36,
+              <br>Jatinegara, Kota Jakarta Timur,<br> Daerah Khusus Ibukota Jakarta 13310<br><br>
+              <strong>Phone:</strong> +62 822 9839 9292<br>
+              <strong>Whatsapp:</strong> <a href="https://wa.me/6282298399292?text=Hallo%20Admin" target="blank">+62 822 9839 9292</a><br>
+              <strong>Email:</strong> admin@muliajaya.xyz<br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
     </div>
 
     <div class="container">
-        <div class="copyright">
-            &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>CV. MULIA JAYA AGUNG</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
     </div>
-</footer><!-- End Footer -->
+  </footer>
+  <!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
